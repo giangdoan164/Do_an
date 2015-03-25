453490d85512e69eb9554c1d4aa64759f1d42931
Doan Hoang Giang <giangdoan164@gmail.com>
2015-03-25 19:00:46 +0700

    add

--- public_html/backup/question.php
+++ public_html/backup/question.php
@@ -4,7 +4,7 @@ http://stackoverflow.com/questions/2954879/how-session-works
     http://www.bennadel.com/blog/70-sql-query-order-of-operations.htm
     http://stackoverflow.com/questions/4596467/order-of-execution-of-the-query
     
-    
+    http://www.sqlviet.com/blog/left-join-voi-menh-de-where
     
     http://vnexpress.net/tin-tuc/goc-nhin/kinh-doanh-kieu-viet-nam-3156586.html
     http://kinhdoanh.vnexpress.net/tin-tuc/tien-cua-toi/nhan-vien-phuc-vu-sai-gon-kiem-hon-chuc-trieu-moi-thang-3161968.html
